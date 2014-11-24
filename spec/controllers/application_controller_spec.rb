66ef37ad6e3da378de1c0fa5f6f90594931948cf
Valentin Aitken <bostko@gmail.com>
2014-11-24 02:48:58 +0200

    Add basic tasks actions

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -5,13 +5,6 @@ describe ApplicationController do
 
   it "should have a current_user" do
     # note the fact that I removed the "validate_session" parameter if this was a scaffold-generated controller
-    subject.current_user.should_not be_nil
-  end
-
-  it "should get index" do
-    # Note, rails 3.x scaffolding may add lines like get :index, {}, valid_session
-    # the valid_session overrides the devise login. Remove the valid_session from your specs
-    get 'tasks'
-    response.should be_success
+    expect(subject.current_user).not_to be_nil
   end
 end
